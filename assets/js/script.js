430fd6f5bb2ef6ff80395ee6ba877bce04394ec4
pawarshivamd <shivam.p@konzeptsolutions.com>
2023-06-19 10:51:07 +0530

    --commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,10 +15,10 @@ window.addEventListener('scroll', function() {
       var swiper = new Swiper(".mySwiper", {
         spaceBetween: 30,
         centeredSlides: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
+        // autoplay: {
+        //   delay: 2500,
+        //   disableOnInteraction: false,
+        // },
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
